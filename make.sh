394fe9364c09621c82c3d494fb7f86cb8b5fd31e
Onur Turhan <onurturhan@gmail.com>
2022-08-12 19:09:13 +0000

    make.sh updated.

--- make.sh
+++ make.sh
@@ -9,3 +9,7 @@ mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON -DBUILD_EXAMPLES=ON ..
 cmake --build . 
+
+
+# ssh-keygen -t rsa -b 4096 -C "onurturhan@gmail.com"
+# cat ~/.ssh/id_rsa.pub
